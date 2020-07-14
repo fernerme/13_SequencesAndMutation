7a89cc577c83c13c0c212acf77eb52852aeda6ab
fernerme <fernerme@users.noreply.github.com>
2020-07-14 17:07:07 -0400

    complete

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -10,8 +10,8 @@ for ITERATING through SEQUENCES, including selections from:
   -- Looking at two sequences in parallel
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Melina Ferner.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -103,9 +103,14 @@ def shortest_string(strings):
       :type strings: list[str]   or tuple(str)
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    index_of_shortest_string = 0
+    for k in range(1, len(strings)):
+        if len(strings[k]) < len(strings[index_of_shortest_string]):
+            index_of_shortest_string = k
+    return strings[index_of_shortest_string]
 
 
 def run_test_index_of_largest_number():
@@ -177,12 +182,16 @@ def index_of_largest_number(numbers, n):
     Type hints:
       :type numbers: list[float]   or tuple[float]
       :type n:       int
-
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    index_of_largest_num = 0
+    for k in range(n):
+        if numbers[index_of_largest_num] < numbers[k]:
+            index_of_largest_num = k
+    return index_of_largest_num
 
 
 # ----------------------------------------------------------------------
@@ -235,9 +244,14 @@ def number_of_stutters(s):
        :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    num_of_stutters = 0
+    for k in range(len(s) - 1):
+        if s[k] == s[k+1]:
+            num_of_stutters = num_of_stutters + 1
+    return num_of_stutters
 
 
 def run_test_is_palindrome():
@@ -308,7 +322,7 @@ def is_palindrome(s):
       :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ####################################################################
@@ -317,6 +331,10 @@ def is_palindrome(s):
     #   above are particularly good examples to work by hand.
     ####################################################################
     # ------------------------------------------------------------------
+    for k in range(len(s)):
+        if s[k] != s[-(k+1)]:
+            return False
+    return True
 
 
 # ----------------------------------------------------------------------
@@ -375,9 +393,14 @@ def count_same(sequence1, sequence2):
       type: sequence2: tuple or list or string
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    number_of_same_items = 0
+    for k in range(len(sequence1)):
+        if sequence1[k] == sequence2[k]:
+            number_of_same_items = number_of_same_items + 1
+    return number_of_same_items
 
 
 # ----------------------------------------------------------------------
